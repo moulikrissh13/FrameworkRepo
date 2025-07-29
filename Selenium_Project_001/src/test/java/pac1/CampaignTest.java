e0102f4d464ec294f5b22485faa70d9c9358aa82
Seemantala Mouli <165631874+moulikrissh13@users.noreply.github.com>
2025-07-29 10:09:02 +0530

    New test script added, commit by mouli

---
+++ Selenium_Project_001/src/test/java/pac1/CampaignTest.java
@@ -0,0 +1,14 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class CampaignTest {
+
+	
+	
+	@Test
+	public void createCamopaignTest() {
+		
+		System.out.println("Created New Campaign");
+	}
+}
